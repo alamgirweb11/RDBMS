b450ec204185f28b8b32be9afc5d0faeff09d69f
alamgirweb11 <alamgirweb11@gmail.com>
2022-08-28 13:29:27 +0600

    display all categories with post

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'home'])->name('home');
 Route::get('/users', [HomeController::class,'users'])->name('users');
+Route::get('/categories', [HomeController::class,'categories'])->name('categories');
