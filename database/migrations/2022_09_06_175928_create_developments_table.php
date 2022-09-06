91a027b21c6b5dab6809a2ee2f77161384df506d
alamgirweb11 <alamgirweb11@gmail.com>
2022-09-07 00:49:45 +0600

    model migration added for has-many-through relationships

---
+++ database/migrations/2022_09_06_175928_create_developments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevelopmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('developments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('environment_id')->constrained();
+            $table->string('commit_hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('developments');
+    }
+}
