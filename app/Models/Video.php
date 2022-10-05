66eeb55a791190fdcfcf968ede5fce8b9deb7476
alamgirweb11 <alamgirweb11@gmail.com>
2022-10-05 23:44:37 +0600

    define has-many-to-many morph relationships

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -10,4 +10,9 @@ class Video extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'title', 'slug', 'source_url', 'video', 'status'];
+
+    public function comments(){
+        return $this->morphToMany(Comment::class, 'commentable');
+     }
+     
 }
